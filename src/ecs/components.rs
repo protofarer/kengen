a200227bdfb4eb210b4e82d1d0ec823faaf9cfd8
Kenny Baron <kennybaron@fastmail.com>
2023-10-08 04:19:44 -0400

    operator overloads was a bad idea, nuke it

--- src/ecs/components.rs
+++ src/ecs/components.rs
@@ -1,9 +1,6 @@
-use crate::logger::Logger;
 use nalgebra::Vector2;
 use std::any::Any;
-use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 use std::sync::atomic::{AtomicU32, Ordering};
-
 static COMPONENT_TYPE_ID: AtomicU32 = AtomicU32::new(1);
 
 pub trait Component: Any {
@@ -28,7 +25,7 @@ macro_rules! impl_component {
     };
 }
 
-struct TransformComponent {
+pub struct TransformComponent {
     position: Vector2<f32>,
     scale: Vector2<f32>,
     rotation: f32,
@@ -58,111 +55,10 @@ pub struct HealthComponent {
 impl_component!(HealthComponent);
 
 impl HealthComponent {
-    fn new(hp: Option<u16>, max_hp: Option<u16>) -> Self {
+    pub fn new(hp: Option<u16>, max_hp: Option<u16>) -> Self {
         Self {
             hp: hp.unwrap_or(100),
             max_hp: max_hp.unwrap_or(100),
         }
-    }
-}
-
-impl<T> Add<T> for HealthComponent
-where
-    T: Add<Output = T> + Into<u16>,
-{
-    type Output = Self;
-
-    fn add(self, rhs: T) -> Self::Output {
-        HealthComponent {
-            hp: self.hp.saturating_add(rhs.into()),
-            max_hp: self.max_hp,
-        }
-    }
-}
-
-impl<T> AddAssign<T> for HealthComponent
-where
-    T: Into<u16>,
-{
-    fn add_assign(&mut self, rhs: T) {
-        self.hp = self.hp.saturating_add(rhs.into());
-    }
-}
-
-impl<T> Sub<T> for HealthComponent
-where
-    T: Sub<Output = T> + Into<u16>,
-{
-    type Output = Self;
-
-    fn sub(self, rhs: T) -> Self::Output {
-        HealthComponent {
-            hp: self.hp.saturating_sub(rhs.into()),
-            max_hp: self.max_hp,
-        }
-    }
-}
-
-impl<T> SubAssign<T> for HealthComponent
-where
-    T: Into<u16>,
-{
-    fn sub_assign(&mut self, rhs: T) {
-        self.hp = self.hp.saturating_sub(rhs.into());
-    }
-}
-
-impl<T> Mul<T> for HealthComponent
-where
-    T: Mul<Output = T> + Into<u16>,
-{
-    type Output = Self;
-
-    fn mul(self, rhs: T) -> Self::Output {
-        HealthComponent {
-            hp: self.hp.saturating_mul(rhs.into()),
-            max_hp: self.max_hp,
-        }
-    }
-}
-
-impl<T> MulAssign<T> for HealthComponent
-where
-    T: Into<u16>,
-{
-    fn mul_assign(&mut self, rhs: T) {
-        self.hp = self.hp.saturating_mul(rhs.into());
-    }
-}
-
-impl<T> Div<T> for HealthComponent
-where
-    T: Div<Output = T> + Into<u16>,
-{
-    type Output = Self;
-
-    fn div(self, rhs: T) -> Self::Output {
-        let divisor = rhs.into();
-        if divisor == 0 {
-            Logger::warn("Entity hp attempted divide by 0");
-            HealthComponent {
-                hp: self.max_hp,
-                max_hp: self.max_hp,
-            }
-        } else {
-            HealthComponent {
-                hp: self.hp.saturating_div(divisor),
-                max_hp: self.max_hp,
-            }
-        }
-    }
-}
-
-impl<T> DivAssign<T> for HealthComponent
-where
-    T: Into<u16>,
-{
-    fn div_assign(&mut self, rhs: T) {
-        self.hp = self.hp.saturating_div(rhs.into());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use kengen::ecs::components::HealthComponent;
 #[allow(warnings, dead_code)]
 use kengen::game::Game;
 
 fn main() {
-    let mut game = Game::new().unwrap_or_else(|e| {
-        println!("{e}");
-        std::process::exit(1);
-    });
+    // let mut game = Game::new().unwrap_or_else(|e| {
+    //     println!("{e}");
+    //     std::process::exit(1);
+    // });
 
-    game.run();
-    game.destroy();
+    // game.run();
+    // game.destroy();
+    let mut c = HealthComponent::new(None, None);
+    c += 640000;
+    println!("{}", c.hp);
 }
